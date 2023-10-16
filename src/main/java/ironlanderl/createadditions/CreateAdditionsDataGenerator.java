e45844abe9418d50cc1972f704003bf1c5d5dd51
ironlanderl <fabrizio03p@gmail.com>
2023-10-16 09:54:06 +0200

    Initial Commit

---
+++ src/main/java/ironlanderl/createadditions/CreateAdditionsDataGenerator.java
@@ -0,0 +1,11 @@
+package ironlanderl.createadditions;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateAdditionsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
